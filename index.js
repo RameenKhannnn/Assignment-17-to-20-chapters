d52e3e4c3a9138310ac6ad7c24c907ab7c2adf01
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-01 22:25:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// pets.shift();
+// pets.unshift("fish", "ferret");
+// pets.splice(2, 2, "pig", "duck", "emu");
+// pets.splice(2, 0, "pig", "duck", "emu");
+// pets.splice(2, 2);
+// var noPets = pets.slice(2, 4);
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// if (cityToCheck === cleanestCities[0]) {
+//      alert("It's one of the cleanest cities");
+//     }
+// else if (cityToCheck === cleanestCities[1]) {
+//     alert("It's one of the cleanest cities");
+//  }
+//      else if (cityToCheck === cleanestCities[2]) {
+//             alert("It's one of the cleanest cities");
+//          }
+//      else if (cityToCheck === cleanestCities[3]) {
+//                  alert("It's one of the cleanest cities");
+//                  }
+//                  else if (cityToCheck === cleanestCities[4]) {
+//                      alert("It's one of the cleanest cities");
+//                      }else {
+                    //     alert("It's not on the list");
+                    //  }
+                    //   for (var i = 0; i <= 4; i++) {
+                    //     if (cityToCheck === cleanestCities[i]) {
+                    //         alert("It's one of the cleanest cities");
+                    //         }
+                    //      }
+// var matchFound = "no";
+//  var matchFound = "no";
+//   for (var i = 0; i <= 4; i++);
+//   if (cityToCheck === cleanestCities[i]) {
+//  matchFound = "yes";
+//  alert("It's one of the cleanest cities");
+//  }
+//     if (matchFound === "no") {
+//     alert("It's not on the list");
+//  }
+
+//  var matchFound = false;
+//  for (var i = 0; i <= 4; i++);
+//  if (cityToCheck === cleanestCities[i]) {
+//      matchFound = true;
+//      alert("It's one of the cleanest cities");
+//     break;
+// }
+//  if (matchFound === false) {
+//      alert("It's not on the list");
+// }
+
+//  var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+
+
+
+
+
+
+
